5184c35d53c6319d51dcb0ce20d4576439e523bc
Shubham <torquecode@gmail.com>
2017-12-29 17:05:10 +0530

    Add booking status response

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import requests
+import config
+
+
+app = Flask(__name__)
+
+
+@app.route('/railbutler', methods=['POST'])
+def find_action():
+    json_data = request.get_json()
+    if json_data['result']['action'] == 'fetch_pnr_status':
+        return fetch_pnr_status(json_data['result']['resolvedQuery'])
+
+
+def fetch_pnr_status(PNR):
+    PNR = PNR.replace('-','')
+    PNR = PNR.replace(' ','')
+    URL = 'https://api.railwayapi.com/v2/pnr-status/pnr/' + PNR + '/apikey/' + config.RAILWAY_API_KEY
+
+    json_response = requests.get(URL, verify=True)
+    json_data = json_response.json()
+
+    speech = ""
+    idx = 1
+    for passenger_detail in json_data["passengers"]:
+        speech += 'Status for passenger ' + str(idx) + ' is '
+        speech += passenger_detail['booking_status']
+        speech += '.\n'
+        idx += 1
+
+    answer = {
+        "speech": speech,
+        "displayText": speech,
+        "source": "fetch_pnr_status"
+    }
+    return jsonify(answer)
